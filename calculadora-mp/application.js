0605891e3df381078bf99655b3394480c496b3d0
Rafael de Camargo <rafacamargo123@gmail.com>
2020-08-03 18:18:16 -0300

    add navigation

--- calculadora-mp/application.js
+++ calculadora-mp/application.js
@@ -4,6 +4,23 @@
 //     .catch(e => console.log(e));
 // }
 
-function documentReady() {
-  document.querySelector('#amount input').focus();
-}
+// window.setTimeout(function() {
+//   document.querySelector('#amount')._input.focus()
+// }, 500);
+
+ons.ready(function() {
+  appNavigator.pushPage('input-amount.html');
+})
+
+document.addEventListener('show', function(event) {
+  var page = event.target;
+
+  if (page.id === 'input-amount') {
+    page.querySelector('#amount')._input.focus();
+    page.querySelector('#goto-select-method').onclick = function() {
+       appNavigator.pushPage('select-method.html', {data: {title: 'Page 2'}});
+    };
+  } else if (page.id === 'page2') {
+    page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+  }
+});
